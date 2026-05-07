bde0d6114e4a662ee00fc8e1c200db25a35817a1
Autocall-1 <zippystay.online@gmail.com>
2026-05-07 14:32:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,12 +28,14 @@ app = FastAPI(
     lifespan=lifespan
 )
 
+# CORS
 origins = [
     "https://autocall-1.github.io",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500"
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://127.0.0.1:5500",
 ]
-    CORSMiddleware,
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
